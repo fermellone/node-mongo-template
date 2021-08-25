7f5a9c6852126229b8a4db8b31a5066482ba7904
femellone <fernandomellone2@gmail.com>
2021-08-25 17:00:11 -0400

    Se agregaron datos mock

---
+++ utils/mocks/items.js
@@ -0,0 +1,36 @@
+const itemsMock = [
+  {
+    id: 'd2a4a062-d256-41bb-b1b2-9d915af6b75e',
+  },
+  {
+    id: '3f951b6d-03fa-4fb0-a7a1-188c0abe9e06',
+  },
+  {
+    id: 'c030eed2-dad1-47f4-b660-9cfb7170eadb',
+  },
+  {
+    id: 'c672ff92-c373-49f4-9c5b-f0151d304361',
+  },
+  {
+    id: '5ec75811-1001-4612-9743-67296d90071a',
+  },
+  {
+    id: '63d718f2-4e37-4090-ad57-dd2cf22393e6',
+  },
+  {
+    id: 'e9093aaa-daf2-4e32-aa6b-c3d79d0d9120',
+  },
+  {
+    id: '94f4fa76-fcf9-4aa3-a18f-f54bb7d462bc',
+  },
+  {
+    id: '9a2e5834-4eb5-4083-8c21-87fdfc0b3b52',
+  },
+  {
+    id: '11fff70c-c312-4791-b8b4-1a0665edd79e',
+  }
+];
+
+module.exports = {
+  itemsMock
+};
