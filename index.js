216da91f3802cedd32a6bdd707f321d42fc57052
femellone <fernandomellone2@gmail.com>
2021-08-25 20:01:03 -0400

    Se agregó la capa de errorhandlers (middlewares)

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 
-const { config } = require('./config/index')
+const { config } = require('./config')
+const itemsApi = require('./routes/items')
 
-const itemssApi = require('./routes/items')
+const { logErrors, errorHandler } = require('./utils/middlewares/errorHandlers')
 
 // body parser
 app.use(express.json())
 
-itemssApi(app)
+itemsApi(app)
+
+app.use(logErrors)
+app.use(errorHandler)
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`);
